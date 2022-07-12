3ce2ad7805300b072ccf6337c6b4595c1f9eb700
charlie <gn4rtistic@gmail.com>
2022-07-11 21:43:14 -0700

    updated server.js file to filter notes with query

--- server.js
+++ server.js
@@ -15,12 +15,26 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 }
 
+function findByTitle(title, notesArray) {
+    const result = notesArray.filter(notes => notes.title === title)[0];
+    return result;
+}
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/notes/:title', (req, res) => {
+    const result = findByTitle(req.params.title, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
