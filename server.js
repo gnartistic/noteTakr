0d7c0a92b77b07a6bd186fcdc5339b546dcf8d7a
charlie <gn4rtistic@gmail.com>
2022-07-11 21:14:06 -0700

    added heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const { notes } = require('./data/notes');
+
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(notes => notes.title === query.title);
+    }
+    if (query.text) {
+        filteredResults = filteredResults.filter(notes => notes.text === query.text);
+    }
+    return filteredResults;
+}
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
