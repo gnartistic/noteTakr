e53f0fd0fc612f15408a9561bd83752cb82177bc
charlie <gn4rtistic@gmail.com>
2022-07-11 21:20:56 -0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 const { notes } = require('./data/notes');
 
